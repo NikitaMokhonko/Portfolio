110c8856259efc294c6832cbaa386b895d189b44
Nikita Mokhonko <nikitamokhonko@gmail.com>
2025-10-21 14:45:42 +0200

    Added Grid Motion component

--- portfolio/src/routes/index.tsx
+++ portfolio/src/routes/index.tsx
@@ -1,12 +1,41 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import GridMotion from "@/components/GridMotion";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: App,
-})
+});
 
 function App() {
+  const items = [
+    "Item 1",
+    <div key="jsx-item-1">Custom JSX Content</div>,
+    "https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "Item 2",
+    <div key="jsx-item-2">Custom JSX Content</div>,
+    "Item 4",
+    <div key="jsx-item-2">Custom JSX Content</div>,
+    "https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "Item 5",
+    <div key="jsx-item-2">Custom JSX Content</div>,
+    "Item 7",
+    <div key="jsx-item-2">Custom JSX Content</div>,
+    "https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "Item 8",
+    <div key="jsx-item-2">Custom JSX Content</div>,
+    "Item 10",
+    <div key="jsx-item-3">Custom JSX Content</div>,
+    "https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "Item 11",
+    <div key="jsx-item-2">Custom JSX Content</div>,
+    "Item 13",
+    <div key="jsx-item-4">Custom JSX Content</div>,
+    "https://images.unsplash.com/photo-1723403804231-f4e9b515fe9d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "Item 14",
+  ];
   return (
-    <div className="text-center">
-    </div>
-  )
+    <>
+      <GridMotion items={items} />
+      <div className="text-center"></div>
+    </>
+  );
 }
