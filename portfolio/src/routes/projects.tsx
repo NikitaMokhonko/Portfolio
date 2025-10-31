e56ac1ed80f6b8f7fe623ef226fb17e1f8224e8c
Nikita Mokhonko <nikitamokhonko@gmail.com>
2025-10-31 22:56:58 +0100

    Added all projects to projects page

--- portfolio/src/routes/projects.tsx
+++ portfolio/src/routes/projects.tsx
@@ -21,7 +21,7 @@ function RouteComponent() {
           <h1 className="text-5xl">My Projects</h1>
         </header>
       </div>
-      <div className="mt-20 px-10 flex flex-row justify-between mb-20">
+      <div className="mt-20 px-10 flex flex-row justify-between mb-20 animate-gentle-pop">
         <ProjectCard
           title="SkinSmart"
           description="Responsive frontend website built for a CS2 business, designed
@@ -54,29 +54,22 @@ function RouteComponent() {
       </div>
       <div className="mt-14 px-10 flex flex-row justify-between mb-20">
         <ProjectCard
+          title="Home EntertAInment System"
+          description="Full-stack AI-powered entertainment platform designed to help users discover and interact with personalized content such as quizzes, movies, and stories through a sleek and responsive interface."
+          img="he-picture.png"
+          to="/home-entertainment"
+        />
+        <ProjectCard
           title="</Salt> AI Assessment"
           description="Full-stack web app built for </Salt> by an 8-person team. Provides clients with an AI-maturity assessment via a dynamic questionnaire, with a modern admin UI and a secure backend for user data."
           img="AI-assessment-picture.png"
           to="/ai-assessment"
         />
         <ProjectCard
-          title="Nova Bank"
-          description="Fully functioning mock banking application built in a team of
-                    11 people, with features such as log in, dashboard, transfers
-                    between accounts and users, transaction history with
-                    AI-powered search, loans and a fully robust admin page."
-          img="Novabank-picture.png"
-          to="/novabank"
-        />
-        <ProjectCard
-          title="Sweethouse"
-          description="Full-stack web application for a French pastry business,
-                    designed to showcase products and provide a seamless browsing
-                    experience for visitors. The site features responsive layouts,
-                    an intuitive UI/UX, and integrated click tracking through
-                    Kafka to gather insights on user interactions."
-          img="Sweethouse-picture.png"
-          to="/sweethouse"
+          title="Portfolio Website"
+          description="This website! A personal portfolio site built to showcase my projects and skills, featuring a clean design, smooth animations, and responsive layouts for a great viewing experience across all devices."
+          img="portfolio-pic.png"
+          to="/portfolio"
         />
       </div>
     </div>
